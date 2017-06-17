f51ea33a19a9b80967e0d0399d8f8cccbfe9f3d6
Joao R C Almeida <joaorca@gmail.com>
2017-06-16 23:23:31 -0300

    Incluindo favicon

--- resources/views/cv.blade.php
+++ resources/views/cv.blade.php
@@ -8,6 +8,7 @@
     <link href="css/bootstrap.min.css" rel="stylesheet">
     <link href="css/font-awesome.css" rel="stylesheet">
     <link href="css/main.css" rel="stylesheet">
+    <link rel="icon" href="img/favicon.ico" type="image/x-icon" />
     <!--[if lt IE 9]>
     <script src="js/html5shiv.js"></script>
     <script src="js/respond.min.js"></script>
